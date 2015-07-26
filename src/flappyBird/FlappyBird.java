58ed6ec820eade6306e0eafb888ef796f415ca73
hilpitome <hilpitome@gmail.com>
2015-07-26 19:14:37 +0300

    created the bird objct

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -1,10 +1,13 @@
 package flappyBird;
 
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
-public class FlappyBird 
+public class FlappyBird implements ActionListener
 {
 	public static FlappyBird FlappyBird;
 	public final int WIDTH=500, HEIGHT=600;
@@ -14,6 +17,7 @@ public class FlappyBird
 		JFrame jframe = new JFrame();
 		
 		renderer = new Renderer();
+		Timer timer = new Timer(20, this);
 		
 		jframe.add(renderer);
 		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
@@ -21,12 +25,18 @@ public class FlappyBird
 		jframe.setResizable(false);
 		jframe.setVisible(true);
 		
-		
+		timer.start();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		renderer.repaint();				
 	}
 	
 	public void repaint(Graphics g) 
 	{
-		// TODO Auto-generated method stub
+		System.out.println("bomm baff");
 		
 	}
  
