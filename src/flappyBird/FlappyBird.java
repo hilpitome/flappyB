bd1d183ebd48c921b06a968cbb8e499c544b82c8
hilpitome <hilpitome@gmail.com>
2015-07-26 18:44:05 +0300

    added renderer class

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -1,20 +1,32 @@
 package flappyBird;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class FlappyBird 
 {
 	public static FlappyBird FlappyBird;
 	public final int WIDTH=500, HEIGHT=600;
+	public Renderer renderer;
 	public FlappyBird()
-	{
+    {
 		JFrame jframe = new JFrame();
 		
+		renderer = new Renderer();
+		
+		jframe.add(renderer);
 		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
 		jframe.setSize(WIDTH, HEIGHT);
 		jframe.setResizable(false);
 		jframe.setVisible(true);
 		
+		
+	}
+	
+	public void repaint(Graphics g) 
+	{
+		// TODO Auto-generated method stub
 		
 	}
  
@@ -24,5 +36,7 @@ public class FlappyBird
 		
 		
 	}
+
+	
 	
 }
